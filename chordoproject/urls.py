5bada86ee6a08b2a8d5bb7dfd59c71bff97d5cad
Aiswarya S <aiswaryaakku9@gmail.com>
2024-12-03 10:48:56 +0530

    settings and urls updated

--- chordoproject/urls.py
+++ chordoproject/urls.py
@@ -20,6 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('adminapp.urls')),
@@ -29,3 +30,7 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_URL)
