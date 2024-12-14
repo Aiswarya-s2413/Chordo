2a74958d5ddcce396e05542ac8cf91eeb00cb5d0
Ubuntu <ubuntu@ip-172-31-8-20.ap-southeast-2.compute.internal>
2024-12-14 05:00:26 +0000

    product view modification

--- userapp/views.py
+++ userapp/views.py
@@ -1158,7 +1158,7 @@ def paymentSuccess(request):
 
 @login_required(login_url='userLogin')
 def trackOrder(request):
-    orders = Order.objects.filter(user=request.user).prefetch_related('order_items__variant').order_by(-id)
+    orders = Order.objects.filter(user=request.user).prefetch_related('order_items__variant').order_by("-created_at")
 
     orders_with_filtered_items = []
 
